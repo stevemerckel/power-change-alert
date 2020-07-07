b4c5fa0057562efa22bea1835043dc3d8ea3c1e3
Steve Merckel <steve.merckel@gmail.com>
2020-07-07 16:09:44 -0500

    adjusted startup logic for cli, service, help.  added commandline output messages.
    ** validated WMI logic working on target laptop **

--- PowerChangeAlerter/Program.cs
+++ PowerChangeAlerter/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using System.ServiceProcess;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using PowerChangeAlerter.Common;
@@ -17,42 +19,76 @@ namespace PowerChangeAlerter
             IFileManager fm = new LocalFileManager();
             IAppLogger logger = new SerilogAppLogger(rs, fm);
             var targetService = new AlerterService(rs, logger, fm);
+
+            // write initialization info
+            var startupMessage = rs.IsLocalDebugging
+                ? "Running with debugging within Visual Studio"
+                : "Running as commandline application manually";
+            logger.Info($"  ***  {startupMessage}  ***  ");
+
+            if (!rs.IsLocalDebugging && args.Length == 0)
+            {
+                // run as windows service
+                var servicesToRun = new ServiceBase[] { targetService };
+                ServiceBase.Run(servicesToRun);
+                return;
+            }
+
+            // allowed args
             const string CommandlineSwitchArg = "CLI";
+            const string HelpShortSwitch = "H";
+            const string HelpLongSwitch = "HELP";
+            var defaultErrorMessage = $"Unable to start.  Use switch '{CommandlineSwitchArg}' to run as commandline, or use '{HelpShortSwitch}' or '{HelpLongSwitch}' for help.";
 
-            if (rs.IsLocalDebugging  || (args.Length == 1 && args[0].Trim().ToUpper().Contains(CommandlineSwitchArg)))
+            if (!rs.IsLocalDebugging && args.Length != 1)
             {
-                // running as console app, and perhaps with a debugger attached
-                var startupMessage = rs.IsLocalDebugging
-                    ? "Running with debugging within Visual Studio"
-                    : "Running as commandline application manually";
-                logger.Info($"  ***  {startupMessage}  ***  ");
-                targetService.StartService();
-                Task.Run(() =>
-                {
-                    targetService.TriggerStandby();
-                    Thread.Sleep(5000);
-                    targetService.TriggerResume();
-                    Thread.Sleep(5000);
-                    targetService.TriggerAcToBattery();
-                    Thread.Sleep(5000);
-                    targetService.TriggerBatteryToAc();
-                    logger.Info("Done with Power tests");
-                });
-                logger.Info("  ***  Press ENTER key to stop program  ***  ");
-                Console.Read();
-                targetService.StopService();
+                // not in local debugging mode and arg list is missing or greater than 1 --> exit with error
+                logger.Error(defaultErrorMessage);
+                Environment.ExitCode = 1;
                 return;
             }
 
-            if (args.Length > 0)
-            {
-                logger.Error($"Unsure how to start.  Either run as an established Windows Service, or use the '{CommandlineSwitchArg}' switch (without quotes) to run as console application.");
-                return;
-            }
+            // grab first arg if it exists
+            var receivedArg = args.Any()
+                ? Regex.Replace(args[0].Trim().ToUpper(), "[^a-zA-Z]", string.Empty)
+                : null;
 
-            // running normally as windows service
-            var servicesToRun = new ServiceBase[] { targetService };
-            ServiceBase.Run(servicesToRun);
+            // decide behavior from arg value
+            switch (receivedArg)
+            {
+                case CommandlineSwitchArg:
+                case null:
+                    RunCommandline(targetService, logger);
+                    break;
+                case HelpShortSwitch:
+                case HelpLongSwitch:
+                    logger.Info($"You can run from commandline with '{CommandlineSwitchArg}' param, or register and run this as a Windows Service.");
+                    break;
+                default:
+                    logger.Error($"Did not recognize argument '{receivedArg}'.  Use switch '{HelpLongSwitch}' for more info.");
+                    Environment.ExitCode = 2;
+                    break;
+            }
+        }
+
+        private static void RunCommandline(AlerterService targetService, IAppLogger logger)
+        {
+            targetService.StartService();
+            Task.Run(() =>
+            {
+                targetService.TriggerStandby();
+                Thread.Sleep(5000);
+                targetService.TriggerResume();
+                Thread.Sleep(5000);
+                targetService.TriggerAcToBattery();
+                Thread.Sleep(5000);
+                targetService.TriggerBatteryToAc();
+                logger.Info("Done with Power tests");
+            });
+            logger.Info("  ***  Press ENTER key to stop program  ***  ");
+            Console.Read();
+            targetService.StopService();
+            return;
         }
     }
 }
