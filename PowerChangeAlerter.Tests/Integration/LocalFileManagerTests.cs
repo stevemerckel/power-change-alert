2d59e5cc5b6e3d0858c3c0f1010099d8b5e13f35
Steve Merckel <steve.merckel@gmail.com>
2020-09-28 16:24:32 -0500

    Squashed commit of the following:
    commit c0dbc300dbf4fae77963b302faa84c2b3e53b9dc
    Author: Steve Merckel <steve.merckel@gmail.com>
    Date:   Mon Sep 28 16:22:18 2020 -0500
    
        resolving and consolidating some test logic

--- PowerChangeAlerter.Tests/Integration/LocalFileManagerTests.cs
+++ PowerChangeAlerter.Tests/Integration/LocalFileManagerTests.cs
@@ -1,13 +1,10 @@
 ﻿using Moq;
 using NUnit.Framework;
 using PowerChangeAlerter.Common;
-using Serilog.Sinks.SystemConsole.Themes;
 using System;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Reflection;
-using System.Security;
 
 namespace PowerChangeAlerter.Tests.Integration
 {
@@ -46,18 +43,18 @@ namespace PowerChangeAlerter.Tests.Integration
         }
 
         [Test]
-        public void Test_DownloadTextFile_Success()
+        public void Test_DownloadTextFile_ValidateContentInspection_Success()
         {
             const string Url = "https://raw.githubusercontent.com/stevemerckel/power-change-alert/master/README.md";
             Console.WriteLine($"Url = {Url}");
-            var randomString = Utility.MakeRandomString(8, false, true);
-            var randomFileLocation = Path.Combine(_workDirectory, $"dummy-download.{randomString}.txt");
+            var downloadFile = Path.Combine(_workDirectory, new Uri(Url).Segments.Last());
+            Console.WriteLine($"{nameof(downloadFile)} = {downloadFile}");
             WebClient wc = null;
             try
             {
                 wc = new WebClient();
                 var startOn = DateTime.Now;
-                wc.DownloadFile(Url, randomFileLocation);
+                wc.DownloadFile(Url, downloadFile);
                 var downloadTimeNeeded = DateTime.Now.Subtract(startOn);
                 Console.WriteLine($"Finished downloading file after {downloadTimeNeeded.TotalMilliseconds} milliseconds");
             }
@@ -66,27 +63,29 @@ namespace PowerChangeAlerter.Tests.Integration
                 wc?.Dispose();
             }
 
-            Assert.IsTrue(_sut.FileExists(randomFileLocation));
-            Assert.IsTrue(new FileInfo(randomFileLocation).Length > 0, $"No content was found in '{randomFileLocation}'");
+            Assert.IsTrue(_sut.FileExists(downloadFile));
+            Assert.IsTrue(new FileInfo(downloadFile).Length > 0, $"No content was found in '{downloadFile}'");
+            Assert.IsFalse(_sut.IsFileBlocked(downloadFile));
             const string KnownSnipInTextFile = "Before Running or Debugging";
-            var fileContent = _sut.ReadAllText(randomFileLocation);
+            var fileContent = _sut.ReadAllText(downloadFile);
             Assert.IsTrue(fileContent.Length > 0);
             Assert.IsTrue(fileContent.Contains(KnownSnipInTextFile, StringComparison.InvariantCulture), $"Could not find known text of '{KnownSnipInTextFile}'");
         }
 
-        [Test]
-        public void Test_DownloadDllFile_Unblock_Success()
+        [TestCase("https://raw.githubusercontent.com/stevemerckel/power-change-alert/master/README.md", false)]
+        [TestCase("https://raw.githubusercontent.com/stevemerckel/power-change-alert/master/resources/naudio.1.10.0.nupkg.zip", true)]
+        public void Test_DownloadFile_UnblockIfSpecified_Success(string url, bool shouldRequireUnblock)
         {
-            const string Url = "https://raw.githubusercontent.com/stevemerckel/power-change-alert/master/resources/NAudio.dll";
-            Console.WriteLine($"Url = {Url}");
-            var randomString = Utility.MakeRandomString(8, false, true);
-            var randomFileLocation = Path.Combine(_workDirectory, $"dummy-download.{randomString}.dll");
+            Console.WriteLine($"{nameof(url)} = {url}");
+            Console.WriteLine($"{nameof(shouldRequireUnblock)} = {shouldRequireUnblock}");
+            var downloadFile = Path.Combine(_workDirectory, new Uri(url).Segments.Last());
+            Console.WriteLine($"{nameof(downloadFile)} = {downloadFile}");
             WebClient wc = null;
             try
             {
                 wc = new WebClient();
                 var startOn = DateTime.Now;
-                wc.DownloadFile(Url, randomFileLocation);
+                wc.DownloadFile(url, downloadFile);
                 var downloadTimeNeeded = DateTime.Now.Subtract(startOn);
                 Console.WriteLine($"Finished downloading file after {downloadTimeNeeded.TotalMilliseconds} milliseconds");
             }
@@ -95,19 +94,17 @@ namespace PowerChangeAlerter.Tests.Integration
                 wc?.Dispose();
             }
 
-            // validate existence
-            Assert.IsTrue(_sut.FileExists(randomFileLocation));
-            Assert.IsTrue(new FileInfo(randomFileLocation).Length > 0, $"No content was found in '{randomFileLocation}'");
+            // initial download validations
+            Assert.IsTrue(_sut.FileExists(downloadFile));
+            Assert.AreEqual(shouldRequireUnblock, _sut.IsFileBlocked(downloadFile));
 
-            // validate file is blocked
-            Assert.IsTrue(_sut.IsFileBlocked(randomFileLocation));
+            // now we can unblock file if it is needed
+            if (_sut.IsFileBlocked(downloadFile))
+                _sut.UnblockFile(downloadFile);
+            Assert.IsFalse(_sut.IsFileBlocked(downloadFile));
 
-            // unblock file and validate we can read the file
-            _sut.UnblockFile(randomFileLocation);
-            Assert.IsFalse(_sut.IsFileBlocked(randomFileLocation));
-            Console.WriteLine("Unblocked the file");
-            var fileContent = _sut.ReadAllText(randomFileLocation);
-            Assert.IsTrue(fileContent.Length > 0);
+            // secondary validations
+            Assert.IsTrue(new FileInfo(downloadFile).Length > 0, $"No content was found in '{downloadFile}'");
         }
 
         [TearDown]
